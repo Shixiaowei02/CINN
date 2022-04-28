bb6818e1f82368df3299f868d37ca8713b1a397a
Shixiaowei02 <39303645+Shixiaowei02@users.noreply.github.com>
2022-04-28 08:57:41 +0000

    update

--- cinn/frontend/pass/dot_merger_test.cc
+++ cinn/frontend/pass/dot_merger_test.cc
@@ -16,6 +16,38 @@
 #include "gtest/gtest.h"
 
 namespace cinn::frontend::pass {
+namespace {
+void CompareResult(Program* program,
+                   const Target& target,
+                   const std::vector<std::string>& input_ids,
+                   const std::vector<std::string>& output_ids,
+                   size_t size_diff,
+                   int seed          = -1,
+                   bool print_tensor = false) {
+  std::unordered_set<std::string> fetch_ids(output_ids.begin(), output_ids.end());
+  // apply common pass
+  ProgramPass::Apply(program, fetch_ids, target, {"Decomposer", "RemoveIdentity"});
+
+  // get original program size
+  auto origin_size = program->size();
+  // get original output
+  auto origin_out = RunProgram(*program, target, input_ids, output_ids, seed, print_tensor);
+
+  // fuse transpose + add + dot, then run and get the fused output
+  ProgramPass::Apply(program, fetch_ids, target, {"TransposeFolding", "GemmRewriter"});
+
+  // get fused program size
+  auto fused_size = program->size();
+  ASSERT_EQ(size_diff, origin_size - fused_size);
+  // get fused output
+  auto fused_out = RunProgram(*program, target, input_ids, output_ids, seed, print_tensor);
+
+  ASSERT_EQ(origin_out.size(), fused_out.size());
+  for (size_t i = 0; i < origin_out.size(); ++i) {
+    ASSERT_FLOAT_EQ(origin_out[i], fused_out[i]);
+  }
+}
+}  // namespace
 
 /*
  * DotMerger Test
