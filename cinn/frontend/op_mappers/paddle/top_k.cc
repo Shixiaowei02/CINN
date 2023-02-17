93e25e94fed3f2924d084a741494c4759428b044
Shixiaowei02 <39303645+Shixiaowei02@users.noreply.github.com>
2023-02-17 06:53:38 +0000

    update

--- cinn/frontend/op_mappers/paddle/top_k.cc
+++ cinn/frontend/op_mappers/paddle/top_k.cc
@@ -17,7 +17,32 @@
 
 namespace cinn {
 namespace frontend {
-namespace paddle_mappers {}  // namespace paddle_mappers
+namespace paddle_mappers {
+
+void TopKOpMapper(const paddle::cpp::OpDesc& op_desc, const OpMapperContext& ctx) {
+  CHECK_EQ(op_desc.Input("X").size(), 1UL);
+  auto x_name = op_desc.Input("X").front();
+  CHECK_EQ(op_desc.Output("Out").size(), 1UL);
+  auto out_name = op_desc.Output("Out").front();
+  CHECK_EQ(op_desc.Output("Indices").size(), 1UL);
+  auto indices_name = op_desc.Output("Indices").front();
+  auto x            = ctx.GetVar(x_name);
+
+  CHECK(op_desc.HasAttr("k"));
+  auto k = utils::GetAttrOrDefault<int>(op_desc, "k");
+
+  auto sort_tmp    = ctx.Builder()->Sort(x, -1, false);
+  auto sort_out    = ctx.Builder()->Slice(sort_tmp, {-1}, {0}, {k});
+  auto argsort_tmp = ctx.Builder()->ArgSort(x, -1, false);
+  auto argsort_out = ctx.Builder()->Slice(argsort_tmp, {-1}, {0}, {k});
+
+  ctx.AddVar(out_name, sort_out);
+  ctx.AddVarModelToProgram(out_name, sort_out->id);
+  ctx.AddVar(indices_name, argsort_out);
+  ctx.AddVarModelToProgram(indices_name, argsort_out->id);
+}
+
+}  // namespace paddle_mappers
 }  // namespace frontend
 }  // namespace cinn
 
