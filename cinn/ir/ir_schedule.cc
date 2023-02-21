c1e8179162654abbb12ea54e57d1bd3fb38629a9
Shixiaowei02 <39303645+Shixiaowei02@users.noreply.github.com>
2023-02-21 10:16:32 +0000

    changes to knonfusible

--- cinn/ir/ir_schedule.cc
+++ cinn/ir/ir_schedule.cc
@@ -1085,7 +1085,6 @@ void ScheduleImpl::SetBuffer(Expr& block, const std::string& memory_type, bool f
 }
 
 void ScheduleImpl::MergeExprs() {
-  LOG(FATAL);
   auto exprs = this->GetModule().GetExprs();
   if (exprs.size() == 1U) return;
   CHECK(exprs[0].As<ir::Block>());
